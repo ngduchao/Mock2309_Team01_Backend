0ea08fc6f55e8dbb4e3c5ffc2e11fdf2046e3648
bykytadev <bykyta@outlook.com.vn>
2023-11-18 21:20:38 +0700

    Thêm Authentication trong createTicket

--- MockProject/src/main/java/com/vti/controller/TicketController.java
+++ MockProject/src/main/java/com/vti/controller/TicketController.java
@@ -4,14 +4,17 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vti.entity.User;
 import com.vti.form.ticket.CreatingTicketForm;
 import com.vti.service.ITicketService;
+import com.vti.service.IUserService;
 
 @CrossOrigin("*")
 @RestController
@@ -23,8 +26,14 @@ public class TicketController {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+	private IUserService userService;
+
     @PostMapping()
-    public ResponseEntity<?> createTicket(@RequestBody CreatingTicketForm form) {
+    public ResponseEntity<?> createTicket(@RequestBody CreatingTicketForm form, Authentication authentication) {
+        String username = authentication.getName();
+        User userInfo = userService.findUserByUsername(username);
+        form.setCreatorId(userInfo.getId());
         service.createTicket(form);
         return new ResponseEntity<String>("Create successfully!", HttpStatus.OK);
     }
