494ea98917a4b98fa31fbe79ea7fafde6f6e3f74
Nguyen Duc Hao <118716382+ngduchao@users.noreply.github.com>
2023-11-25 00:18:42 +0700

    Update WebSecurityConfiguration.java

--- MockProject/src/main/java/com/vti/config/authentication/WebSecurityConfiguration.java
+++ MockProject/src/main/java/com/vti/config/authentication/WebSecurityConfiguration.java
@@ -3,12 +3,9 @@ package com.vti.config.authentication;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.context.annotation.Bean;
-=======
 
 import org.springframework.context.annotation.Configuration;
->>>>>>> 05e0f3adf855167a4a3462692b9013eceaea1c7b
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,15 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-<<<<<<< HEAD
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-=======
->>>>>>> 05e0f3adf855167a4a3462692b9013eceaea1c7b
-
-
 
 import com.vti.service.IUserService;
 
@@ -72,7 +64,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		;
 	}
 	
-<<<<<<< HEAD
 	@Bean
     CorsConfigurationSource corsConfigurationSource() {
 		final CorsConfiguration configuration = new CorsConfiguration();
@@ -83,29 +74,3 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-=======
-//	@Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//		final CorsConfiguration configuration = new CorsConfiguration();
-//	    configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-//	    configuration.applyPermitDefaultValues();
-//	    
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return (CorsConfigurationSource) source;
-//    }
-	
-//	@Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-//        configuration.setAllowCredentials(true);
-//        configuration.addAllowedHeader("X-Requested-With");
-//        configuration.addAllowedHeader("Content-Type");
-//        org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
->>>>>>> 05e0f3adf855167a4a3462692b9013eceaea1c7b
-}
