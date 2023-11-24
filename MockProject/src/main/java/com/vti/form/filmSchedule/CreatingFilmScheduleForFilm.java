53bcddb0eb04ac0df3d8cef46d406c426cf0b2ba
Nguyen Duc Hao <118716382+ngduchao@users.noreply.github.com>
2023-11-25 00:20:38 +0700

    Update CreatingFilmScheduleForFilm.java

--- MockProject/src/main/java/com/vti/form/filmSchedule/CreatingFilmScheduleForFilm.java
+++ MockProject/src/main/java/com/vti/form/filmSchedule/CreatingFilmScheduleForFilm.java
@@ -10,18 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class CreatingFilmScheduleForFilm extends CreatingFilmSchedule{
-<<<<<<< HEAD
 	
 	@NotNull(message = "The film id mustn't be null value")
-=======
-	@NotNull
->>>>>>> 05e0f3adf855167a4a3462692b9013eceaea1c7b
 	@FilmIDExists
 	private Integer filmId;
-	
-	
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> 05e0f3adf855167a4a3462692b9013eceaea1c7b
